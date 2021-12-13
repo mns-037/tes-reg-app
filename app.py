e3e69c7d6ec5960d6bee62762ae895f3d0319772
Muhammad Nanda <muhns037@gmail.com>
2021-12-13 20:09:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ import pickle
 
 # Load necessary file
 model = pickle.load(open('model.pkl', 'rb'))
-
+st.write('# Deskripsi App')
+st.write('Ini adalah deployment web app dari model regresi untuk prediksi asuransi')
  
-with st.form('For Assurance Prediction'):
-  st.markdown('## Enter the input')
-  news_input = st.text_area(' ', ' ')
+with st.form('Prediksi Asuransi'):
+  st.markdown('## Masukkan Input')
   tingkat_pelayanan = st.selectbox('Tingkat Pelayanan', options=['30 (Rawat Inap)', '40 (Rawat Jalan'])# categorical
   jumlah_peserta_aktif = st.number_input('Jumlah Peserta Aktif Periode Ini:', step=0.1, min_value=0.0, format='%.f')# float
-  submitted = st.form.form_submit_button('Prediksi Asuransi')
-  
+  submitted = st.form_submit_button('Lakukan Prediksi')
   
   if submitted:
     st.info('Hasil Predikisi: ') #apa??
