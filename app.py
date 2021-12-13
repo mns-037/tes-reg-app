561e9000c9dcbcb1efd4988dda391f1ca7231529
Muhammad Nanda <muhns037@gmail.com>
2021-12-13 21:30:53 +0700

    updt

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pickle
 st.set_page_config(page_title='Aplikasi Regresi Asuransi', layout='wide')
 
 # Load necessary file
-model = pickle.load(open('model.pkl', 'rb'))
-#scl  = pickle.load(open('scl.pkl', 'rb'))
+model = pickle.load(open('model_baru.pkl', 'rb'))
+scl  = pickle.load(open('scl.pkl', 'rb'))
 
 # Title
 st.write('# Deskripsi App')
@@ -17,14 +17,18 @@ st.write('Ini adalah deployment web app dari model regresi untuk prediksi asuran
 with st.form('Prediksi Asuransi'):
   st.markdown('## Masukkan Input')
   tingkat_pelayanan = st.selectbox('Tingkat Pelayanan', options=['30 (Rawat Inap)', '40 (Rawat Jalan'])# categorical
+  tingkat_pelayanan_ = 40
+  if tingkat_pelayanan=='30 (Rawat Inap)':
+    tingkat_pelayanan_ = 30
+  
   jumlah_peserta_aktif = st.number_input('Jumlah Peserta Aktif Periode Ini:', step=0.1, min_value=0.0, format='%.f')# float
   submitted = st.form_submit_button('Lakukan Prediksi')
   
   if submitted:
-    # input_data = pd.DataFrame({'x1': [tingkat_pelayanan], 'x2':[jumlah_peserta_aktif]})
-    #input_data = np.asarray([tingkat_pelayanan, jumlah_peserta_aktif])
-    #scaled_input_data = input_data #scl.transform(input_data)
-    #st.info('Hasil Prediksi: {}'.format(model.predict(scaled_input_data))) #apa??
+    # input_data = pd.DataFrame({'x1': [tingkat_pelayanan_], 'x2':[jumlah_peserta_aktif]})
+    input_data = np.asarray([[tingkat_pelayanan_, jumlah_peserta_aktif]])
+    scaled_input_data = scl.transform(input_data)
+    st.info('Hasil Prediksi: {}'.format(model.predict(scaled_input_data))) #apa??
     st.info('Coefficient: {}'.format(model.coef_))
     st.info('Intercept: {}'.format(model.intercept_))
     st.info('n_feature: {}'.format(model.n_features_in_))
