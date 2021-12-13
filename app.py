6337dc3b4af1efa5e1f8fd99038bbe98617fdce6
Muhammad Nanda <muhns037@gmail.com>
2021-12-13 20:05:16 +0700

    updt

--- app.py
+++ app.py
@@ -8,34 +8,16 @@ import pickle
 # Load necessary file
 model = pickle.load(open('model.pkl', 'rb'))
 
-news_categories = ['sport', 'business', 'politics', 'tech', 'entertainment']
-news_categories_imgs = ['sport.png', 'business.png', 'politics.jpg', 'tech.jpg', 'entertainment.jpg']
-
-
-# Function to clean text
-def clean_text(text, stp_word):
-  text = text.strip()
-  text = text.lower()
-  text = text.translate(str.maketrans('','',string.punctuation))
-  text = ' '.join([word for word in text.split() if word not in stp_word])
-  return text
-
  
 with st.form('For Assurance Prediction'):
   st.markdown('## Enter the input')
   news_input = st.text_area(' ', ' ')
+  tingkat_pelayanan = st.selectbox('Tingkat Pelayanan', options=['30 (Rawat Inap)', '40 (Rawat Jalan'])# categorical
+  jumlah_peserta_aktif = st.number_input('Jumlah Peserta Aktif Periode Ini:', step=0.1, min_value=0.0, format='%.f')# float
+  submitted = st.form.form_submit_button('Prediksi Asuransi')
   
-  submitted = st.form.form_submit_button('Category Prediction')
   
   if submitted:
-    news_input = clean_text(news_input, stp_word)
-#	news_input_to_seq = tokenizer.texts_to_sequences(news_input)
-#	pad_news_input_to_seq = pad_sequences(seq, padding='post', maxlen=120)
-#	news_pred = model.predict(pad_news_input_to_seq)
-#	idx = np.argmax(pred)-1
-#	pred_lbl = news_categories[idx]
-#	image = Image.open(dir+news_categories_imgs[idx])
-#	st.markdown('### Prediction:')
-#	st.image(image, caption=pred_lbl)
-	
+    st.info('Hasil Predikisi: ') #apa??
+	st.info(model)
 	
